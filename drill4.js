4dfc9e5cc5be8b9bc1d35747f01bcf7d650f6764
John McDowell <orioles4@hotmail.com>
2017-03-22 18:42:11 +0000

    lesson 7 final commit

--- drill4.js
+++ drill4.js
@@ -18,8 +18,9 @@ function display(event) {
 //displays and animates the next element after the current target
 function display2(event) {
     
-     $(event.currentTarget).next().animate( {height: 'toggle'}, "slow");
-     
+    $(event.currentTarget).next().animate( {height: 'toggle'}, "slow");
+
+    
 }//end of display2
 
 //attach event listener to h3 elements to invoke display function when clicked
@@ -27,9 +28,9 @@ $("h3").click(display2);
 
 //change the background color h3 element when mouse hovers over it
 $("h3").hover(function(){
-     $(this).css("background-color", "yellow");
-     }, function(){
-     $(this).css("background-color", "pink");
+    $(this).css("background-color", "yellow");
+    }, function(){
+    $(this).css("background-color", "pink");
 });
 
 //hover() will trigger display2 method each time mouse hovers over header
